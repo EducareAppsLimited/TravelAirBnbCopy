2d036f463271a9a6d78813fee0beda38c8cf0ef1
sauravMir <saurav_41@yahoo.com>
2020-01-05 03:36:06 +0900

    the header is done

---
+++ components/HeaderLogin.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="row">
+    <strong>Ultimate Tourism</strong>
+    <h1 class= "divMoveRight">bsabs</h1>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.divMoveRight {
+
+float: right;
+}
+</style>
